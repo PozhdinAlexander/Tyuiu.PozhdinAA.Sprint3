dc13d63acbbf16e87a38b452714d23c38f012e6e
Tyuiu.PozhdinAA@outlook.com <strimerkiy@bk.ru>
2023-10-20 01:04:36 +0500

    Task2

---
+++ Tyuiu.PozhdinAA.Sprint3.Task2.V24.Lib/DataService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using tyuiu.cources.programming.interfaces.Sprint3;
+
+namespace Tyuiu.PozhdinAA.Sprint3.Task2.V24.Lib
+{
+    public class DataService : ISprint3Task2V24
+    {
+        public double GetMultiplySeries(double value, int startValue, int stopValue)
+        {
+            double res = 1;
+
+            do
+            {
+                res *= ((Math.Pow(value, startValue) + 4) * Math.Cos(value));
+                startValue++;
+            }
+            while (startValue <= stopValue);
+
+            return Math.Round(res, 3);
+        }
+    }
+}
---
+++ Tyuiu.PozhdinAA.Sprint3.Task2.V24/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.PozhdinAA.Sprint3.Task2.V24.Lib;
+
+namespace Tyuiu.PozhdinAA.Sprint3.Task2.V24
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #3 | Выполнил: Пождин А. А. | АСОиУБ-23-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #3                                                               *");
+            Console.WriteLine("* Тема: Алгоритмы циклической структуры                                   *");
+            Console.WriteLine("* Задание #2                                                              *");
+            Console.WriteLine("* Вариант #24                                                             *");
+            Console.WriteLine("* Выполнил: Пождин Александр Андреевич | АСОиУБ-23-1                     *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу используя цикл do...while, которая вычисляет         *");
+            Console.WriteLine("* произведение ряда по формуле, при a = 0,25.                             *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("*      8    k                                                             *");
+            Console.WriteLine("*  p = П  (a  + 4) * cos(a)                                               *");
+            Console.WriteLine("*     k=1                                                                 *");
+            Console.WriteLine("*                                                                         *");
+
+            Console.WriteLine("***************************************************************************");
+
+            double a = 0.25;
+            int startValue = 1;
+            int stopValue = 8;
+
+
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine($"a = {a}");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine(ds.GetMultiplySeries(a, startValue, stopValue));
+            Console.ReadKey();
+        }
+    }
+}
